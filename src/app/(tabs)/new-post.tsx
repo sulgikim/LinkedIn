d0f115098ea7ff9e42e5a31caf53f1b4421391d3
sulgikim <92609054+sulgikim@users.noreply.github.com>
2023-08-23 15:05:51 -0700

    New Post screen

--- src/app/(tabs)/new-post.tsx
+++ src/app/(tabs)/new-post.tsx
@@ -1,28 +1,111 @@
-import { StyleSheet } from 'react-native';
+import { Text ,TextInput , View, Image, StyleSheet, Pressable } from 'react-native';
+import userJson from '../../../assets/data/user.json'; 
+import { useLayoutEffect, useState } from 'react';
 
-import { Text, View } from '@/components/Themed';
+import { User } from '@/types';
+import { useNavigation, useRouter } from 'expo-router';
+
+type UserProps = {
+  user: User;
+}
 
 export default function NewPostScreen() {
+
+  const [content, setContent] = useState('');
+
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  const onPost = () => {
+    console.warn(`Posting: `, content);
+
+    router.push('/(tabs)/'); //redirect to home screen 
+    setContent('');
+  };
+
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'New Post',
+      //headerLeft: () => <Image source = {{uri : user.image}} style={styles.image} />,
+      headerRight: () => (
+        <Pressable onPress={onPost} style={styles.postButton}> 
+          <Text style={styles.postButtonText}>Submit</Text>
+        </Pressable>
+      ), 
+    }); //check _layout.tsx file 
+  }, [onPost]); 
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>New Post Screen</Text>
+      {/* Header */}
+      {/* <View style={styles.header}> 
+        <Image 
+          source = {{uri : user.image}}
+          style={styles.image}
+        />
+        <Text style={styles.toWho}> Anyone </Text>
+        <Pressable  onPress={onPost}>
+          <Text> Post </Text>
+        </Pressable>
+      </View> */}
+
+      {/* Content */}
+      {/* <Text style={styles.placeholder}> What do you want to talk about? </Text> */}
+
+      <TextInput 
+        value={content}
+        onChangeText={setContent}
+        placeholder='What do you want to talk about?'
+        style={styles.input}
+        multiline
+      />
+
+
+      {/* Bottom Buttons */}
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
+    backgroundColor: 'white',
+    height: '100%',
   },
-  title: {
-    fontSize: 20,
+  //header 
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    marginBottom: 15, 
+  },
+  image: {
+    width: 50, 
+    aspectRatio: 1,
+
+  },
+  toWho: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: 'gray',
+  },
+  postButton: {
+    padding: 5, 
+    paddingHorizontal: 15,
+    backgroundColor: 'royalblue',
+    borderRadius: 50,
+    marginRight: 10,
+  },
+  postButtonText: {
+    color: 'white',
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+
+  //content
+  input: {
+    fontSize: 18, 
+  }
 });
